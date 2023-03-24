c83d9bd80cf5c6f4f6582655d138b0bc412bdbaa
206922 <206922@KB-120-C02>
2023-03-24 09:18:56 +0300

    Добавьте файлы проекта.

---
+++ TestSystem/MainWindow/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
+
+namespace TestSystem
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Closing += ShowCloseMessage;
+
+            var timer = new System.Windows.Threading.DispatcherTimer();
+            timer.Interval = new TimeSpan(0, 0, 1);
+            timer.IsEnabled = true;
+            timer.Tick += (o, e) => { CurrentTime.Text = DateTime.Now.ToString(); };
+            timer.Start();
+
+        }
+        private void ShowCloseMessage(object sender, CancelEventArgs e)
+        {
+            if (ActionConfirmation("Вы уверены, что хотите закрыть приложение?") == MessageBoxResult.No) e.Cancel = true;
+        }
+        private static MessageBoxResult ActionConfirmation(string question) => MessageBox.Show(question, "Подтвердите действие", MessageBoxButton.YesNo, MessageBoxImage.Question);
+    }
+}
