b2607a1cd884b4dcaab76f3e328dcd329e8828eb
Owner <artemdem972@gmail.com>
2023-04-06 00:26:00 +0300

    DarkTheme

--- TestSystem/Resources/WindowStyle/WindowStyle.cs
+++ TestSystem/Resources/WindowStyle/WindowStyle.cs
@@ -1,9 +1,38 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
 
-namespace Project.Style.Window
+namespace CustomWindowStyle
 {
     partial class WindowStyle
     {
+        public enum Themes { Light, Dark };
+        public static void ChangeDefaultTheme(Themes theme)
+        {
+            System.Windows.Window me = Application.Current.MainWindow;
+            CheckBox cb = me.Template.FindName("ChangeThemeCheckBox", me) as CheckBox;
+            if (theme == Themes.Light)
+            {
+                cb.IsChecked = false;
+            }
+            else
+            {
+                cb.IsChecked = true;
+            }
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            (sender as System.Windows.Window).StateChanged += Window_StateChanged;
+        }
+        private void Window_StateChanged(object sender, EventArgs e)
+        {
+            System.Windows.Window me = (sender as System.Windows.Window);
+            Button maximizeCaptionButton = me.Template.FindName("MaxRestoreButton", me) as Button;
+            if (maximizeCaptionButton != null)
+            {
+                maximizeCaptionButton.Content = me.WindowState == WindowState.Maximized ? "2" : "1";
+            }
+        }
         private void CloseButton_Click(object sender, RoutedEventArgs e)
         {
             ((sender as FrameworkElement).TemplatedParent as System.Windows.Window).Close();
@@ -18,6 +47,22 @@ namespace Project.Style.Window
         {
             ((sender as FrameworkElement).TemplatedParent as System.Windows.Window)
                 .WindowState = WindowState.Minimized;
+        }
+        private void ToggleSwitchChecked(object sender, RoutedEventArgs e)
+        {
+            var uri = new Uri("Resources\\Dictionaries\\DarkTheme.xaml", UriKind.Relative);
+            ResourceDictionary resourceDict = Application.LoadComponent(uri) as
+            ResourceDictionary;
+            Application.Current.Resources.Clear();
+            Application.Current.Resources.MergedDictionaries.Add(resourceDict);
+        }
+        private void ToggleSwitchUnchecked(object sender, RoutedEventArgs e)
+        {
+            var uri = new Uri("Resources\\Dictionaries\\LightTheme.xaml", UriKind.Relative);
+            ResourceDictionary resourceDict = Application.LoadComponent(uri) as
+            ResourceDictionary;
+            Application.Current.Resources.Clear();
+            Application.Current.Resources.MergedDictionaries.Add(resourceDict); ;
         }
     }
 }
